f124f968fc00a135b449614b3a79954909c60b1c
Rizqika <rizqikaibnu@gmail.com>
2024-11-06 06:04:20 +0700

    1:44:34-(clerk auth)

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,16 +7,66 @@ import InputField from "@/components/InputField"
 import CustomButton from "@/components/CustomButton"
 import OAuth from "@/components/OAuth"
 
+import { useSignUp } from "@clerk/clerk-expo"
+
+import { ReactNativeModal } from 'react-native-modal'
+
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp()
+
   const [form, setForm] = useState({
     name: '',
     email: '',
     password: ''
   });
 
-  const onSignUpPress = async () => {
+  const [verification, setVerification] = useState({
+    state: 'success',
+    error: '',
+    code: ''
+  })
 
-  };
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      })
+
+      await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
+
+      setVerification({
+        ...verification,
+
+        state: 'pending'
+      })
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
+
+  const onPressVerify = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code
+      })
+
+      if (completeSignUp.status === 'complete') {
+        await setActive({ session: completeSignUp.createdSessionId })
+        setVerification({ ...verification, state: 'success' })
+      } else {
+        setVerification({ ...verification, error: 'Verification Failed.', state: 'failed' })
+      }
+    } catch (err: any) {
+      setVerification({ ...verification, error: err.errors[0].longMessage, state: 'failed' })
+    }
+  }
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -79,7 +129,14 @@ const SignUp = () => {
             <Text className="text-primary-500">Log In</Text>
           </Link>
 
-          {/* Verification Modal */}
+          <ReactNativeModal isVisible={verification.state === "success"}>
+            <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+              <Image
+                source={images.check}
+                className="w-[110px] h-[110px] mx-auto my-5"
+              />
+            </View>
+          </ReactNativeModal>
           
         </View>
       </View>
