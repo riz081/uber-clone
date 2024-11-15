6af2ff84bd86af5247f963766391964b1d93e013
Rizqika <rizqikaibnu@gmail.com>
2024-11-15 17:27:36 +0700

    2:26:14 - (HomePage UI)

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -10,6 +10,7 @@ import OAuth from "@/components/OAuth"
 import { useSignUp } from "@clerk/clerk-expo"
 
 import { ReactNativeModal } from 'react-native-modal'
+import { fetchAPI } from "@/lib/fetch"
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp()
@@ -60,6 +61,16 @@ const SignUp = () => {
       })
 
       if (completeSignUp.status === 'complete') {
+        // Create a database user 
+        await fetchAPI('/(api)/user', {
+          method: 'POST',
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          })
+        })
+
         await setActive({ session: completeSignUp.createdSessionId })
         setVerification({ ...verification, state: 'success' })
       } else {
